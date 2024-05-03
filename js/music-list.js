6ad28c62e25102de0f7197cb0bacbec61aecdcb6
Rohit Sharma <82994772+rohitvlog7524@users.noreply.github.com>
2024-05-03 17:43:50 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+    {
+      name: "Heeriye-Arijit Singh",
+      artist: "Arijit Singh",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Leke Prabhu Ka Naam",
+      artist: "Arijit Singh",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "Ram Siya Ram",
+      artist: "Sachet-Parampara",
+      img: "music-3",
+      src: "music-3"
+    },
+    {
+      name: "Kabhi Shaam Dhale",
+      artist: "Mohammad Faiz",
+      img: "music-4",
+      src: "music-4"
+    },
+    {
+      name: "Pahadan Di Hoor",
+      artist: "Rinku Mansar Bala",
+      img: "music-5",
+      src: "music-5"
+    },
+    {
+      name: "Kaljuein Peed",
+      artist: "Amit Mittu",
+      img: "music-6",
+      src: "music-6"
+    },
+];
